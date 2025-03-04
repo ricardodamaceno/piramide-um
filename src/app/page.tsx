32bdd7a5a92759a82878f4d4ec3dd7aa7f26ebf0
Ricardo Oliveira <ricardodamaceno92@gmail.com>
2025-03-04 17:56:18 -0300

    fet: login home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,22 @@
+"use client"
+
+import { Button } from "@/components/ui/button";
+import { useRouter } from 'next/navigation';
+
 export default function Home() {
+  const router = useRouter();
   return (
-    <main>
-      Bem-vindo ao meu projeto!
+    <main className="min-h-screen flex items-center justify-center">
+      <div className="flex flex-col items-center space-y-4">
+        <h1>Bem-vindo ao meu projeto!</h1>
+        <div>
+        <Button
+          className="my-4" 
+          onClick={() => router.push('/auth')}>
+          Login
+        </Button>
+        </div>
+      </div>
     </main>
   );
 }
--- src/components/auth/LogoutButton.tsx
+++ src/components/auth/LogoutButton.tsx
@@ -1,11 +1,12 @@
 "use client"
 
 import { signOut } from "next-auth/react";
+import { Button } from "../ui/button";
 
 export default function LogoutButton() {
   return (
-    <button onClick={() => signOut()}>
+    <Button onClick={() => signOut()}>
        SAIR 
-    </button>
+    </Button>
   );
 }
